fecfa48d2a32535d040c316a6b5672b31b6f3bca
OleksanderHovriashchyk <oleksandrhovrasuk@gmail.com>
2022-05-14 02:48:57 +0300

    Here my task 3

---
+++ Lesson 1/Task 3/Task 3/Task 3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 0, answer = 0;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 11) 
+    {
+        cout << "Do you study? (1/0): ";
+        cin >> answer;
+        if (answer == 1)
+        {
+            cout << "You are awesome!";
+        }
+        else 
+        {
+            cout << "Why not?";
+        }
+    }
+    else 
+    {
+        cout << "Wow, you are so big";
+    }
+}
