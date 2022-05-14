da8ce2c8567c18f0eab6f87ef40fd4d6df204fd9
OleksanderHovriashchyk <oleksandrhovrasuk@gmail.com>
2022-05-14 14:39:50 +0300

    Here my task 5

---
+++ Lesson 1/Task 5/Task 5/Task 5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int b = 0, a = 0, c = 0, D = 0;
+    cout << "Please enter b: ";
+    cin >> b;
+    cout << "Please enter a: ";
+    cin >> a;
+    cout << "Please enter c: ";
+    cin >> c;
+
+    D = pow(b,2) - 4 * a * c;
+
+    cout << "Your discriminant is ";
+
+    if (D == 0) 
+    {
+        c = -b / (2 * a);
+        cout << "equale 0 you have 1 root - " << c;
+    }
+    if (D > 0)
+    {
+        c = (-b + sqrt(D)) / (2 * a);
+        D = (-b - sqrt(D)) / (2 * a);
+        cout << "higher than zero, you have 2 root, the first one is - " << c << endl
+            << "The second one is - " << D;
+    }
+    else
+        cout << "lower than zero, you have 0 roots";
+}
