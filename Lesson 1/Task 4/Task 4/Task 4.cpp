8e4b1a3b3ce05af07e24ace07d6eacb4fd31d5f4
OleksanderHovriashchyk <oleksandrhovrasuk@gmail.com>
2022-05-14 04:06:57 +0300

    Here my task 4

---
+++ Lesson 1/Task 4/Task 4/Task 4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cost = 0, price = 0;
+    float discount = 0;
+    
+    cout << "Motherboard price: ";
+    cin >> cost;
+    price += cost;
+    
+    cout << "Videocard price: ";
+    cin >> cost;
+    price += cost;
+    
+    cout << "CPU price: ";
+    cin >> cost;
+    price += cost;
+    
+    cout << "Discount: ";
+    cin >> discount;
+    discount = discount / 100;
+
+    price -= price * discount;
+
+    cout << "Youe PC will cost - " << price;
+}
