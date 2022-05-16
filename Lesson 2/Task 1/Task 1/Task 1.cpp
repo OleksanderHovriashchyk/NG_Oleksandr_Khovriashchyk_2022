50ae72f5974343baf6001a2f308199f0aab38cdd
OleksanderHovriashchyk <oleksandrhovrasuk@gmail.com>
2022-05-16 10:24:48 +0300

    Here my task 1 in lesson 2

---
+++ Lesson 2/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size = 0;
+    cout << "Please enter the size of your square: ";
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        for (int f = 0; f < size; f++)
+        {
+            if ((i >= 1 && i <= size - 2) && (f >= 1 && f <= size - 2))
+                cout << " ";
+            else
+                cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
