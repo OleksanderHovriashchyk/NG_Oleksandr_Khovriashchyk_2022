28853d8a8ceb58ad3100817fa55dbd15834380dd
OleksanderHovriashchyk <oleksandrhovrasuk@gmail.com>
2022-05-16 10:26:18 +0300

    Here my task 2

---
+++ Lesson 2/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0, sum = 0;
+    cout << "Enter your number, please: ";
+    cin >> number;
+    cout << "Sum of even number are: ";
+    
+    for (int i = number; i > 0; i /= 10)
+    {
+        if ((i % 2 == 0) && (i != 0))
+        {
+            if (sum != 0)
+                cout << " + ";
+            
+            sum += i % 10;
+            cout << i % 10;
+        }
+    }
+    cout << " = " << sum;
+}
